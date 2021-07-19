93572c03cc2bf89b4657d338a5d2e3ee401b0f68
ZeeTom <zthom13@hotmail.com>
2021-07-19 13:06:48 -0600

    created Cupcake class

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 """Flask app for Cupcakes"""
+
+from models import Cupcake, db, connect_db
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+app.config['SECRET_KEY'] = "gfudhiaskhjl543278489grhuiger8934"
--- models.py
+++ models.py
@@ -1,1 +1,36 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+DEFAULT_URL = 'https://tinyurl.com/demo-cupcake'
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Table of cupcake instance"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, 
+        primary_key=True,
+        autoincrement=True)
+
+    flavor = db.Column(db.Text,
+        nullable=False)
+
+    size = db.Column(db.Text,
+        nullable=False)
+
+    rating = db.Column(db.Integer,
+        nullable=False)
+
+    image = db.Column(db.Text,
+        nullable=False,
+        default=DEFAULT_URL)
+
+    def __repr__(self):
+        return f'< Cupcake. {self.id}, {self.flavor}, {self.size}, {self.rating}. >'
